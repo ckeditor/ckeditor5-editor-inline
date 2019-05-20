0ba550f2f8da4a788003aa27e197505b7835ef7f
Mateusz Samsel <m.samsel@cksource.com>
2019-05-20 11:51:26 +0200

    Add unit test for toolbar's label.

--- tests/inlineeditorui.js
+++ tests/inlineeditorui.js
@@ -52,6 +52,14 @@ describe( 'InlineEditorUI', () => {
 			expect( view.isRendered ).to.be.true;
 		} );
 
+		it( 'renders accessibility label for toolbar', () => {
+			const labelEl = view.toolbar.element.lastChild;
+
+			expect( labelEl.tagName ).to.equal( 'SPAN' );
+			expect( labelEl.classList.contains( 'ck-toolbar__label' ) ).to.be.true;
+			expect( labelEl.innerText ).to.equal( 'Editor\'s toolbar' );
+		} );
+
 		describe( 'panel', () => {
 			it( 'binds view.panel#isVisible to editor.ui#focusTracker', () => {
 				ui.focusTracker.isFocused = false;
